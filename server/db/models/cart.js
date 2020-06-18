7e6666a4da4edcadb82967fb08474faa1bafe375
Maria <marybelousova@yahoo.com>
2020-06-18 15:48:50 -0400

    updated

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -4,11 +4,11 @@ const db = require('../db')
 const Cart = db.define('cartMask', {
   quantity: {
     type: Sequelize.INTEGER,
-    allownull: false,
+    allownNull: false,
     validate: {
       min: 0
     },
-    dafaultValue: 1
+    defaultValue: 1
   },
   price: {
     type: Sequelize.INTEGER,
