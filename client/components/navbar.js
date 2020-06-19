e9fbd0fb087eddaaca4fe1b15006c5eb680aee6d
alesin <alesintipler@gmail.com>
2020-06-18 22:26:24 -0400

    refactored navbar to avoid conflicts with new Cart components & state

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
@@ -34,34 +34,52 @@ const useStyles = makeStyles(theme => ({
 
 const Navbar = ({handleClick, isLoggedIn}) => {
   const classes = useStyles()
-  const [anchorEl, setAnchorEl, anchorElCart, setAnchorElCart] = React.useState(
-    null
-  )
-  const open = Boolean(anchorEl)
-  const openCart = Boolean(anchorElCart)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const isMenuOpen = Boolean(anchorEl)
 
   const handleProfileMenuOpen = event => {
     setAnchorEl(event.currentTarget)
   }
-  const handleCartMenuOpen = event => {
-    setAnchorElCart(event.currentTarget)
-  }
 
   const handleMenuClose = () => {
     setAnchorEl(null)
-    setAnchorElCart(null)
   }
+
+  const menuId = 'menu-appbar-user'
+  const renderUserMenu = (
+    <Menu
+      id={menuId}
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem button component={Link} to="/profile" onClick={handleMenuClose}>
+        Profile
+      </MenuItem>
+      <MenuItem
+        className="Logout"
+        onClick={() => {
+          handleClick()
+          handleMenuClose()
+        }}
+      >
+        Logout
+      </MenuItem>
+    </Menu>
+  )
 
   return (
     <div>
       <h1>MASKS R US</h1>
-      {/* <div>
-      <Icon color="secondary">search</Icon>
-      <Icon color="secondary">shopping_cart</Icon>
-      <Icon color="primary">add_shopping_cart</Icon>
-      <Icon>account_circle</Icon>
-      <Icon>login</Icon>
-    </div> */}
 
       <nav>
         <div className={classes.root}>
@@ -79,54 +97,19 @@ const Navbar = ({handleClick, isLoggedIn}) => {
               <Typography variant="h6" className={classes.title}>
                 Masks R Us
               </Typography>
-              {isLoggedIn ? (
-                <div>
-                  {/* The navbar will show these links after you log in */}
-                  {/* <Link to="/home" className={classes.appBarLink}>Home</Link> */}
-                  <div>
-                    <IconButton
-                      aria-label="account of current user"
-                      aria-controls="menu-appbar-user"
-                      aria-haspopup="true"
-                      onClick={handleProfileMenuOpen}
-                      color="inherit"
-                    >
-                      <Icon>account_circle</Icon>
-                    </IconButton>
 
-                    <Menu
-                      id="menu-appbar-user"
-                      anchorEl={anchorEl}
-                      anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right'
-                      }}
-                      keepMounted
-                      transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right'
-                      }}
-                      open={open}
-                      onClose={handleMenuClose}
-                    >
-                      {/* <MenuItem onClick={handleProfileClick}>Profile</MenuItem> */}
-                      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-                      <MenuItem onClick={handleClick}>Logout</MenuItem>
-                    </Menu>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                {isLoggedIn ? <Icon>account_circle</Icon> : null}
+              </IconButton>
 
-                    <IconButton
-                      aria-label="number of items in cart"
-                      color="inherit"
-                    >
-                      <Badge badgeContent={3} color="secondary">
-                        <Icon color="inherit">shopping_cart</Icon>
-                      </Badge>
-                    </IconButton>
-                  </div>
-                </div>
-              ) : (
+              {isLoggedIn ? null : (
                 <div>
-                  {/* The navbar will show these links before you log in */}
                   <Button color="inherit">
                     <Link to="/login" className={classes.appBarLink}>
                       Login
@@ -137,41 +120,17 @@ const Navbar = ({handleClick, isLoggedIn}) => {
                       Sign Up
                     </Link>
                   </Button>
-                  <IconButton
-                    aria-label="show items in cart"
-                    aria-controls="menu-appbar-cart"
-                    aria-haspopup="true"
-                    onClick={handleCartMenuOpen}
-                    color="inherit"
-                  >
-                    <Badge badgeContent={3} color="secondary">
-                      <Icon color="inherit">shopping_cart</Icon>
-                    </Badge>
-                  </IconButton>
-
-                  <Menu
-                    id="menu-appbar-cart"
-                    anchorEl={anchorElCart}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right'
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right'
-                    }}
-                    open={openCart}
-                    onClose={handleMenuClose}
-                  >
-                    {/* <MenuItem onClick={handleProfileClick}>Profile</MenuItem> */}
-                    <MenuItem onClick={handleMenuClose}>Edit Cart</MenuItem>
-                    <MenuItem onClick={handleMenuClose}>Checkout</MenuItem>
-                  </Menu>
                 </div>
               )}
+
+              <IconButton aria-label="number of items in cart" color="inherit">
+                <Badge badgeContent={isLoggedIn ? 15 : 4} color="secondary">
+                  <Icon color="inherit">shopping_cart</Icon>
+                </Badge>
+              </IconButton>
             </Toolbar>
           </AppBar>
+          {isLoggedIn ? renderUserMenu : null}
         </div>
       </nav>
       <hr />
@@ -188,21 +147,21 @@ const mapState = state => {
   }
 }
 
-const mapDispatch = (dispatch, ownProps) => {
+const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout())
-    },
-    handleProfileClick: () => {
-      ownProps.history.push('/home')
     }
+    // handleProfileClick: () => {
+    //   ownProps.history.push('/profile')
+    // }
   }
 }
 
 export default withRouter(connect(mapState, mapDispatch)(Navbar))
 
 /**
- * PROP TYPES
+ //* PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
