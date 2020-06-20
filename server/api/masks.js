44333e2548867025856bdafef9391ebce33bb725
Chidi <cokeke1@gmail.com>
2020-06-20 17:26:18 -0400

    add cents to dollar conversions to backend as opposed to frontend for masks

--- server/api/masks.js
+++ server/api/masks.js
@@ -5,6 +5,9 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const masks = await Mask.findAll()
+    masks.forEach(mask => {
+      mask.price = mask.price / 100
+    })
     res.json(masks)
   } catch (error) {
     next(error)
@@ -14,6 +17,8 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   try {
     const singleMask = await Mask.findByPk(req.params.id)
+    singleMask.price = singleMask.price / 100
+    console.log(singleMask)
     res.json(singleMask)
   } catch (error) {
     next(error)
