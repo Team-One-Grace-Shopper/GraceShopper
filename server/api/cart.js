5bb0ec646e087efdf670a4829e5e65f414baee19
alesin <alesintipler@gmail.com>
2020-06-20 22:05:27 -0400

    notes on how to construct cart api routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const {Mask, Order, User, Cart} = require('../db/models')
 module.exports = router
 
+// *** GET a user's cart  (api/cart/)
 router.get('/:userId', async (req, res, next) => {
   try {
     const findCurrentCart = await Order.findOne({
@@ -21,9 +22,16 @@ router.get('/:userId', async (req, res, next) => {
   }
 })
 
-router.post('/', async (req, res, next) => {
+// *** ADD new mask to a user's cart  'api/cart/' (make connection to order with status "cart")
+
+// *** CREATE new order (with status = cart)
+
+// *** UPDATE cart (quantity)
+
+router.post('/:userId', async (req, res, next) => {
   try {
     const order = await Order.addOrCreateOrder(req.user.id)
+    // const currentCart = await userInstance.addOrCreateOrder()
     if (order !== undefined) {
       order.addItemToOrder(req.body.id, order.id)
     }
@@ -33,6 +41,7 @@ router.post('/', async (req, res, next) => {
   }
 })
 
+// *** SUBMIT order (get current price of mask (from mask model) to update $ in cart model, calculate order total, mark order as "placed")
 router.put('/submit', async (req, res, next) => {
   try {
     if (req.user !== undefined) {
