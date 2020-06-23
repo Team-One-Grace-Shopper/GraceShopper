4c8fdf6c79670c186fa0098c67eb3f0c02d38c81
alesin <alesintipler@gmail.com>
2020-06-23 12:10:57 -0400

    navbar update to represent new cart

--- client/components/thanks.js
+++ client/components/thanks.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {getCart} from '../store/cart'
 
-function Thanks() {
-  return (
-    <div>
-      <h2>Thank you for shopping! Your order is confirmed!</h2>
-    </div>
-  )
+class Thanks extends React.Component {
+  componentDidMount() {
+    this.props.getCart(this.props.userId)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Thank you for shopping! Your order is confirmed!</h2>
+      </div>
+    )
+  }
 }
 
-export default Thanks
+const mapState = state => ({
+  userId: state.user.id
+})
+
+const mapDispatch = dispatch => {
+  return {
+    getCart: userId => dispatch(getCart(userId))
+  }
+}
+
+export default connect(mapState, mapDispatch)(Thanks)
