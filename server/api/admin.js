4627b75c0f06bcf00a76179819bd0d306531121c
Maria <marybelousova@yahoo.com>
2020-06-23 16:44:20 -0400

    admin - get router fix

--- server/api/admin.js
+++ server/api/admin.js
@@ -14,15 +14,16 @@ function isAdmin(req, res, next) {
 router.get('/', isAdmin, async (req, res, next) => {
   try {
     const allOrders = await Order.findAll({
-      where: {
-        attributes: ['data', 'total', 'price']
-      },
+      // where: {
+      //   attributes: ['date', 'total', 'status']
+      // },
       include: {
-        model: Mask,
-        attributes: ['id', 'name', 'price']
+        model: User,
+        attributes: ['firstName', 'lastName', 'email']
       }
     })
-  } catch {
+    res.json(allOrders)
+  } catch (error) {
     next(error)
   }
 })
@@ -31,6 +32,29 @@ router.post('/', isAdmin, async (req, res, next) => {
   try {
     const newMask = await Mask.create(req.body)
     res.json(newMask)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/masks/:id', isAdmin, async (req, res, next) => {
+  try {
+    const foundMask = await Mask.findByPK(req.params.id)
+    const updatedMask = await foundMask.update(req.body)
+    res.json(updatedMask)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/masks/:id', isAdmin, async (req, res, next) => {
+  try {
+    await Mask.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.sendStatus(200)
   } catch (error) {
     next(error)
   }
