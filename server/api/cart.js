442b1ba8f8baa3c8906a33805f7b2e64eb024fe5
Maria <marybelousova@yahoo.com>
2020-06-23 22:58:15 -0400

    fixed isLogging middleware

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,8 +2,17 @@ const router = require('express').Router()
 const {Mask, Order, User, Cart} = require('../db/models')
 module.exports = router
 
+const isLogging = (req, res, next) => {
+  if (req.user === undefined || req.user.id !== Number(req.params.userId)) {
+    const error = new Error('illegal action')
+    error.status = 401
+    return next(error)
+  }
+  next()
+}
+
 // *** GET a user's cart  (api/cart/)
-router.get('/:userId', async (req, res, next) => {
+router.get('/:userId', isLogging, async (req, res, next) => {
   try {
     const currentCart = await Order.findOne({
       where: {
@@ -26,7 +35,7 @@ router.get('/:userId', async (req, res, next) => {
 })
 
 // *** ADD new mask to a user's cart  'api/cart/' (make connection to order with status "cart")
-router.post('/:userId/addToCart/:maskId', async (req, res, next) => {
+router.post('/:userId/addToCart/:maskId', isLogging, async (req, res, next) => {
   try {
     if (req.params.userId !== 0) {
       const userCart = await Order.findOne({
@@ -44,6 +53,7 @@ router.post('/:userId/addToCart/:maskId', async (req, res, next) => {
 
 router.post(
   '/:userId/:orderId/update/:maskId',
+  isLogging,
   async (req, res, next) => {
     try {
       const [NumOfAffectedRows, affectedRows] = await Cart.update(req.body, {
@@ -64,7 +74,7 @@ router.post(
 )
 
 // *** SUBMIT order (get current price of mask (from mask model) to update $ in cart model, calculate order total, mark order as "placed", update the order DATE, create new order with status "cart")
-router.put('/:userId/submit', async (req, res, next) => {
+router.put('/:userId/submit', isLogging, async (req, res, next) => {
   try {
     if (req.params.userId) {
       const foundOrder = await Order.findOne({
@@ -101,7 +111,7 @@ router.put('/:userId/submit', async (req, res, next) => {
 })
 
 // *** Deleting an item from a cart
-router.delete('/:orderId/remove/:maskId', async (req, res, next) => {
+router.delete('/:orderId/remove/:maskId', isLogging, async (req, res, next) => {
   try {
     const userCart = await Order.findByPk(req.params.orderId)
     if (userCart) {
